57b2be1fb6263bb16405d5426a0af5175eb9bcc3
weilewei <lokwei9@gmail.com>
2019-09-24 01:09:38 -0400

    modify cmake and modules load files w.r.t netlib-lapack and openblas

---
+++ build-aux/summit_load_modules_hpx_openblas.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# Loads all modules that are required to build DCA++ on ORNL's Summit supercomputer.
+# A reset is done at the beginning to restore to the default programming environment on Summit.
+#
+# Usage: source summit_load_modules.sh
+
+module reset
+module load gcc/8.1.1
+module load cuda
+module load hdf5
+module load fftw
+module load cmake
+export OLCF_MAGMA_ROOT="/gpfs/alpine/cph102/proj-shared/lib/magma-2.5.1-gcc8"
+module load netlib-lapack
+module load openblas/0.3.6-omp
+module load essl
+
+export CC=mpicc
+export CXX=mpicxx
+export CFLAGS=-fPIC
+export CXXFLAGS="-fPIC -mcpu=power9 -mtune=power9 -ffast-math"
+export LDFLAGS=-dynamic
+export LDCXXFLAGS="$LDFLAGS"
