679f90965b2592c06d921226b7850c049b965e48
Weile <lokwei9@gmail.com>
2021-03-15 11:41:18 -0400

    update summit build

--- build-aux/summit.cmake
+++ build-aux/summit.cmake
@@ -34,6 +34,8 @@ set(CMAKE_CUDA_ARCHITECTURES 70 CACHE STRING "GPU Hardware Architecture.")
 
 # Summit's static CUDA runtime is bugged.
 option(CUDA_USE_STATIC_CUDA_RUNTIME OFF)
+# Enable adios2 support 
+option(DCA_WITH_ADIOS2 ON)
 
 set(MAGMA_DIR "/gpfs/alpine/proj-shared/cph102/epd/magma_cuda11" CACHE PATH
   "Path to the MAGMA installation directory. Hint for CMake to find MAGMA.")
--- build-aux/summit_load_modules.sh
+++ build-aux/summit_load_modules.sh
@@ -7,13 +7,14 @@
 
 module reset
 module load gcc/8.1.1
-module load cuda
-module load magma/2.5.1
+module load cuda/11.1.0
+module load magma/2.5.4-cuda11.1
 module load hdf5
 module load fftw
 module load cmake
 module load netlib-lapack
 module load essl
+module load adios2/2.7.0
 
 export CC=mpicc
 export CXX=mpicxx
